dfb80982ef9dbc77c212a445917c7563b9fb74db
unknown <624473119@qq.com>
2024-06-16 21:18:14 +0800

    使用cesium

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,57 @@ const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const productionGzipExtensions = ["js", "css"];
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 console.log("process.env", process.env);
 
 module.exports = {
   publicPath: "./",
   lintOnSave: false, // 关闭语法检查
-  chainWebpack: (config) => {
+  configureWebpack: {
+    plugins: [
+      // Copy Cesium Assets, Widgets, and Workers to a static directory
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "node_modules/cesium/Build/Cesium/Workers",
+            to: "Workers"
+          },
+          {
+            from: "node_modules/cesium/Build/Cesium/ThirdParty",
+            to: "ThirdParty"
+          },
+          {
+            from: "node_modules/cesium/Build/Cesium/Assets",
+            to: "Assets"
+          },
+          {
+            from: "node_modules/cesium/Build/Cesium/Widgets",
+            to: "Widgets"
+          }
+        ]
+      }),
+      new webpack.DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify("")
+      })
+    ],
+    module: {
+      // Removes these errors: "Critical dependency: require function is used in a way in which dependencies cannot be statically extracted"
+      // https://github.com/AnalyticalGraphicsInc/cesium-webpack-example/issues/6
+      unknownContextCritical: false,
+      unknownContextRegExp: /\/cesium\/cesium\/Source\/Core\/buildModuleUrl\.js/,
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto"
+        }
+      ]
+    }
+  },
+  chainWebpack: config => {
     new CompressionWebpackPlugin({
       algorithm: "gzip",
       test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
@@ -25,7 +69,7 @@ module.exports = {
       })
     );
 
-    // config.plugin("bundleAnalyzerPlugin").use(new BundleAnalyzerPlugin());
+    // config.plugin("bundleAnalyzerPlugin").use(new BundleAnalyzerPlugin()); // 解开可查看项目打包分布情况
   },
   devServer: {
     disableHostCheck: true,
